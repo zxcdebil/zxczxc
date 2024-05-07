f5e00a62586f9527e5c3c3c27631a043692b25e7
zxcdebil <168092816+zxcdebil@users.noreply.github.com>
2024-05-07 16:09:39 +0300

    Add files via upload

---
+++ zxc_pipa_lebedevArtem/zxc_pipa_lebedevArtem/Room.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zxc_pipa_lebedevArtem
+{
+    public class Room
+    {
+        double roomLength;
+        double roomWidth; 
+        public double RoomLength
+    {
+        get { return roomLength; }
+        set { roomLength = value; }
+    }
+     public double RoomWidth
+     {
+        get { return roomWidth; }
+        set { roomWidth = value; }
+     }
+        /// <summary>
+        /// метод вычесления периметр комнаты
+        /// </summary>
+        /// <returns>Возращает значения периметра</returns>
+        public double RoomPerimetr()
+     {
+        return 2 * (roomLength + roomWidth);
+     }
+     /// <summary>
+     /// метод вычесления площадь комнаты
+     /// </summary>
+     /// <returns>Возращает значения площади</returns>
+     public double RoomArea()
+     {
+            return roomLength * roomWidth;
+     }
+        /// <summary>
+        /// метод вычесляет число квадратных метров
+        /// на одного человека 
+        /// </summary>
+        /// <param name="np">Число человек</param>
+        /// <returns>Возращает число квадратых метров</returns>
+     public double PersonArea(int np)
+     {
+            return RoomArea() / np;
+     }
+    }
+   
+}
+
+
+
